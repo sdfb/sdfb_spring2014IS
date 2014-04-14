89f36eaa1b3c806f0b4f69c605882219ca3cd298
Ratanalert Chanamon <cratanal@andrew.cmu.edu>
2014-04-14 16:15:53 -0400

    sidebar changed to accordion style -- confidence interval slider implemented but buggy

---
+++ js/accordion.js
@@ -0,0 +1,32 @@
+/*jQuery time*/
+// $(document).ready(function(){
+// 	$("#accordion h3").click(function(){
+// 		//slide up all the link lists
+// 		$("#accordion ul ul").slideUp();
+// 		//slide down the link list below the h3 clicked - only if its closed
+// 		if(!$(this).next().is(":visible"))
+// 		{
+// 			$(this).next().slideDown();
+// 		}
+// 	})
+// })
+
+$(document).ready(function(){
+	//change colors when hover
+	$("#accordion h3").hover($(this).addClass('hover'), $(this).removeClass('hover'));
+	$("#accordion h3").click(function(){
+		//change colors when click
+		$('#accordion h3').css('background', '#196B94');
+		$('#accordion h3').removeClass('on');
+		$(this).css('background','#185D80');
+		$(this).addClass('on');
+
+		//slide up all the link lists
+		$("#accordion div").slideUp();
+		//slide down the link list below the h3 clicked - only if its closed
+		if(!$(this).next().is(":visible"))
+		{
+			$(this).next().slideDown();
+		}
+	})
+})
