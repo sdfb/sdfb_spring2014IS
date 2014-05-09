2556bfe7506b9582f670382bc5faac088706a8e2
Francis <sdfb.cmu@gmail.com>
2014-05-09 10:54:39 -0400

    show confidence at result header

--- js/display.js
+++ js/display.js
@@ -263,7 +263,7 @@ function showOneNode(id, confidence, data) {
 	keys[p.id] = { "id": p.id, "text": p.label, "cluster": getCluster(p.birth), "size": 14 };
 	for (n in keys) { nodes.push(keys[n]); }
 	$('#graph').html('');
-	$("#results").html("Two degrees of <b>" + p.name +"</b>");
+	$("#results").html("Two degrees of <b>" + p.name +"</b> at" + p.conf);
 
 	var options = { width: $("#graph").width(), height: $("#graph").height(), colors: getColors() };
 	var graph = new Insights($("#graph")[0], nodes, edges, options).render();
